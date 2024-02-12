bc3e607cf81159fde4493eb5b60cefdbb4aa9ce1
DroidStudioAS <as.droiddevteam@gmail.com>
2024-02-12 11:49:31 +0100

    added primary key to room url

--- chat_app/base/urls.py
+++ chat_app/base/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
-    path('room/', views.secondPage, name='room'),
+    path('room/<str:pk>/', views.secondPage, name='room'),
 ]
--- chat_app/base/views.py
+++ chat_app/base/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
 # Create your views here.
-def home(request):
-    return render(request, 'home.html')
 
-def secondPage(request):
-    return render(request, 'rooms.html')
+rooms = [
+    {'id':1, "name": 'lets learn python'},
+    {'id': 2, "name": 'lets learn php'},
+    {'id': 3, "name": 'lets python'}
+
+]
+
+def home(request):
+    return render(request, 'base/home.html', {"rooms":rooms})
+
+def secondPage(request, pk):
+    return render(request, 'base/room.html')
